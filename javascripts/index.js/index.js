1af660f1e502cec8fcc0890166d6a3620188ba25
Trdhurba <Trdhurba12@gmail.com>
2022-08-17 10:18:04 -0700

    main

--- javascripts/index.js/index.js
+++ javascripts/index.js/index.js
@@ -1,1 +1,55 @@
-alert('hello world!')
+/** globals  **/
+
+/** node Getter **/
+const baseUrl ='Http://localhost:3000';
+letcat
+const mainDiv =() =>document.getElementById("main")
+const homepagelink = () => document.get elementById('home-page-link')
+const list catlink = document.getElementById(list cat link)
+/** template **/
+ 
+const homepageTemplate =() => {
+    return
+    <h1 class="center-align">welcome to our cat tracker</h1>
+
+}
+    
+const listcattemplate =() => {
+    return
+}
+ 
+const rendershomepage = () => {
+    mainDiv().innerHTML =homepageTemplate();
+}
+/** Events **/
+const loadcat =() => {
+    fetch(baseUrl+ '/cat')
+}
+ .then(resp => resp.json())
+ .then(data => cat =data)
+
+document.addEventlistener(Domcontentloaded, () => {
+    //rendersHomepage();
+    homepagelinkEvent()
+    meallistlinkEvent()
+})
+
+
+
+
+
+
+
+
+
+/**when the Dom loads **/
+const rendersposts =async () = {
+    let uri ='http://localhost:3000/post';
+    const res=await fetch(uri);
+    const posts =await res.json()
+    console.log(posts);
+}
+document.addEventListener('DOMContentLoaded'), () => rendershomepage();
+
+
+
